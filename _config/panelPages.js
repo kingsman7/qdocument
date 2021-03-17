b970a6a89b6519fa94653e010556aeb274babe00
msolanogithub <msolano2505@gmail.com>
2021-03-17 15:34:41 -0500

    +Add: Structure to `ipanel`

---
+++ _config/panelPages.js
@@ -0,0 +1,17 @@
+export default {
+  documents: {
+    permission: 'idocs.documents.manage',
+    activated: true,
+    authenticated: true,
+    path: '/document/documents',
+    name: 'qdocument.panel.documents',
+    crud : import('@imagina/qdocument/_crud/documents'),
+    page: () => import('@imagina/qcrud/_pages/admin/crudPage'),
+    layout: () => import('@imagina/qsite/_layouts/master'),
+    title: 'qdocument.sidebar.adminDocuments',
+    icon: 'fas fa-folder-open',
+    subHeader: {
+      refresh: true,
+    }
+  }
+}
---
+++ _config/panelSidebar.js
@@ -0,0 +1,12 @@
+const pages = config('pages') // Get Pages from config
+
+//Blog
+export default [
+  {
+    title: 'qdocument.sidebar.adminGroup',
+    icon: 'fas fa-folder-open',
+    children: [
+      pages.qdocument.documents, // posts setup
+    ]
+  },
+]

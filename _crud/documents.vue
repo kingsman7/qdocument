ae6e43863c94f1202837fc1a89f8aea20026e680
vicPereira <victoracho.box@gmail.com>
2022-02-19 16:24:28 -0400

    agregando boton de descarga en el crud de documents

--- _crud/documents.vue
+++ _crud/documents.vue
@@ -3,187 +3,169 @@
 export default {
   data() {
     return {
-      crudId: this.$uid(),
-    };
+      crudId: this.$uid()
+    }
   },
   computed: {
     crudData() {
       return {
         crudId: this.crudId,
         entityName: config("main.qdocument.entityNames.document"),
-        apiRoute: "apiRoutes.qdocument.documents",
-        permission: "idocs.documents",
-        extraFormFields: "idocs.crud-fields.documents",
+        apiRoute: 'apiRoutes.qdocument.documents',
+        permission: 'idocs.documents',
+        extraFormFields: 'idocs.crud-fields.documents',
         create: {
-          title: this.$tr("qdocument.layout.newDocument"),
+          title: this.$tr('idocs.cms.newDocument'),
         },
         read: {
           columns: [
             {
-              name: "id",
-              label: this.$tr("ui.form.id"),
-              field: "id",
+              name: 'id', label: this.$tr('isite.cms.form.id'),
+              field: 'id',
               sortable: true,
             },
             {
-              name: "title",
-              label: this.$tr("ui.form.title"),
-              field: "title",
-              align: "left",
+              name: 'title', label: this.$tr('isite.cms.form.title'),
+              field: 'title',
+              align: 'left',
               sortable: true,
             },
             {
-              name: "status",
-              label: this.$tr("ui.form.status"),
-              field: "status",
-              align: "left",
-              asStatus: true,
+              name: 'status', label: this.$tr('isite.cms.form.status'), field: 'status', align: 'left',
+              asStatus: true
             },
             {
-              name: "private",
-              label: this.$tr("ui.form.type"),
-              field: "private",
-              align: "left",
-              format: (val) =>
-                val
-                  ? parseInt(val)
-                    ? this.$tr("ui.label.private")
-                    : this.$tr("ui.label.public")
-                  : "",
+              name: 'private', label: this.$tr('isite.cms.form.type'), field: 'private', align: 'left',
+              format : val => val ? (parseInt(val) ? this.$tr('isite.cms.label.private') : this.$tr('isite.cms.label.public')) : ''
             },
             {
-              name: "category",
-              label: this.$tr("ui.form.category"),
-              align: "left",
-              field: "category",
-              sortable: true,
-              format: (val) => (val && val.title ? val.title : "-"),
+              name: 'category', label: this.$tr('isite.cms.form.category'), align: 'left',
+              field: 'category', sortable: true, format: val => ((val && val.title) ? val.title : '-')
             },
             {
-              name: "createdAt",
-              label: this.$tr("ui.form.createdAt"),
-              field: "createdAt",
-              format: (val) => (val ? this.$trd(val) : "-"),
-              align: "left",
-              sortable: true,
+              name: 'createdAt', label: this.$tr('isite.cms.form.createdAt'), field: 'createdAt',
+              format: val => val ? this.$trd(val) : '-',
+              align: 'left', sortable: true
             },
             {
-              name: "actions",
-              label: this.$tr("ui.form.actions"),
-              align: "center",
+              name: 'actions', label: this.$tr('isite.cms.form.actions'), align: 'center'
             },
           ],
+          requestParams: {include: 'category'},
           actions: [
             {
               label: this.$tr("isite.cms.label.download"),
               format: (item) => {
-                console.warn(item);
                 return { vIf: item.url ? true : false }
               },
               icon: "fas fa-file-download",
               action: (item) => this.$helper.downloadFromURL(item.url),
             }
           ],
-          requestParams: { include: "category" },
         },
         update: {
-          title: this.$tr("qdocument.layout.updateDocument"),
-          requestParams: { include: "users" },
+          title: this.$tr('idocs.cms.updateDocument'),
+          requestParams: {include: 'users'}
         },
         delete: true,
         formLeft: {
-          id: { value: "" },
-          key: { value: null },
+          id: {value: ''},
+          key: {value: null},
           title: {
-            value: "",
-            type: "input",
+            value: '',
+            type: 'input',
             isTranslatable: true,
             props: {
-              label: `${this.$tr("ui.form.title")}*`,
-              rules: [(val) => !!val || this.$tr("ui.message.fieldRequired")],
+              label: `${this.$tr('isite.cms.form.title')}*`,
+              rules: [
+                val => !!val || this.$tr('isite.cms.message.fieldRequired')
+              ],
             },
           },
           description: {
-            value: "",
-            type: "html",
+            value: '',
+            type: 'html',
             isTranslatable: true,
             props: {
-              label: `${this.$tr("ui.form.description")}*`,
-              rules: [(val) => !!val || this.$tr("ui.message.fieldRequired")],
-            },
-          },
+              label: `${this.$tr('isite.cms.form.description')}*`,
+              rules: [
+                val => !!val || this.$tr('isite.cms.message.fieldRequired')
+              ],
+            }
+          }
         },
         formRight: {
           categoryId: {
             value: null,
-            type: "crud",
+            type: 'crud',
             props: {
-              crudType: "select",
-              crudData: import("@imagina/qdocument/_crud/categories"),
+              crudType: 'select',
+              crudData: import('@imagina/qdocument/_crud/categories'),
               crudProps: {
-                label: `${this.$tr("ui.form.category")}*`,
-                rules: [(val) => !!val || this.$tr("ui.message.fieldRequired")],
+                label: `${this.$tr('isite.cms.form.category')}*`,
+                rules: [
+                  val => !!val || this.$tr('isite.cms.message.fieldRequired')
+                ],
               },
             },
           },
           status: {
-            value: "1",
-            type: "select",
+            value: '1',
+            type: 'select',
             props: {
-              label: this.$tr("ui.form.status"),
+              label: this.$tr('isite.cms.form.status'),
               options: [
-                { label: this.$tr("ui.label.enabled"), value: "1" },
-                { label: this.$tr("ui.label.disabled"), value: "0" },
-              ],
+                {label: this.$tr('isite.cms.label.enabled'), value: '1'},
+                {label: this.$tr('isite.cms.label.disabled'), value: '0'},
+              ]
             },
           },
           private: {
-            value: "0",
-            type: "select",
+            value: '0',
+            type: 'select',
             props: {
-              label: this.$tr("ui.form.type"),
+              label: this.$tr('isite.cms.form.type'),
               options: [
-                { label: this.$tr("ui.label.private"), value: "1" },
-                { label: this.$tr("ui.label.public"), value: "0" },
-              ],
+                {label: this.$tr('isite.cms.label.private'), value: '1'},
+                {label: this.$tr('isite.cms.label.public'), value: '0'},
+              ]
             },
           },
           users: {
             value: [],
-            type: "select",
+            type: 'select',
             props: {
-              label: this.$tr("ui.label.shareWith"),
+              label: this.$tr('isite.cms.label.shareWith'),
               multiple: true,
-              useChips: true,
+              useChips: true
             },
             loadOptions: {
-              apiRoute: "apiRoutes.quser.users",
-              select: { label: "fullName", id: "id" },
-            },
+              apiRoute: 'apiRoutes.quser.users',
+              select: {label: 'fullName', id: 'id'}
+            }
           },
           mediasSingleFile: {
-            name: "mediasSingle",
+            name: 'mediasSingle',
             value: {},
-            type: "media",
+            type: 'media',
             props: {
-              label: `${this.$tr("ui.label.file")}*`,
-              zone: "file",
+              label: `${this.$tr('isite.cms.label.file')}*`,
+              zone: 'file',
               entity: "Modules\\Idocs\\Entities\\Document",
               entityId: null,
-              disk: "privatemedia",
+              disk: 'privatemedia',
               rules: [
-                (val) =>
-                  (val && val.file) || this.$tr("ui.message.fieldRequired"),
+                val => (val && val.file) || this.$tr('isite.cms.message.fieldRequired')
               ],
-            },
-          },
+            }
+          }
         },
-      };
+      }
     },
     //Crud info
     crudInfo() {
-      return this.$store.state.qcrudComponent.component[this.crudId] || {};
-    },
+      return this.$store.state.qcrudComponent.component[this.crudId] || {}
+    }
   },
-};
+}
 </script>
